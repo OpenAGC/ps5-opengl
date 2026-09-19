d4e1d803475866528788cb87b39528a1a6ef77f0
BlackBearReloaded <blackbearreloaded@protonmail.com>
2026-09-18 22:33:41 -0400

    Move PSBC compile state off worker stacks

--- tests/ps5/compute_glsl_fixtures.h
+++ tests/ps5/compute_glsl_fixtures.h
@@ -292,7 +292,7 @@
     "third_party/mesa-26.2.0/src/compiler/shader_info.h": "b7c0ffcdc60db695854fa596171c8bffe7928598fe93d1cb17939114b7f4e74b",
     "third_party/mesa-26.2.0/src/mesa/program/prog_statevars.c": "3c5d09c2d5f5a0747ea77e3c413a2f5661e54d2787bd4183a61c602001537643",
     "third_party/mesa-26.2.0/src/mesa/state_tracker/st_glsl_to_nir.cpp": "f8e1695bd04d4f304be651aa49b9ae1150997aa025cce7a489127a0c718c7d2e",
-    "third_party/opengnm-psbc/libpsbc/psbc_compile.c": "daca613a6e6a3e0985026d4e3fb3d1cb432105cda6e361fd2f28d8be2b6769bd",
+    "third_party/opengnm-psbc/libpsbc/psbc_compile.c": "8e029e4b6ca22724b67e30f32484c5234c9ab2833ceec8fe67d5abc80c92d354",
     "third_party/opengnm-psbc/libpsbc/psbc_compile.h": "732a803c2e29f173ce69c75bc8f23137bec17c02ee4717e4be4ddb89f99f7ac8",
     "third_party/opengnm-psbc/src/amd/common/nir/ac_nir.c": "bf93d76a158bfbf5fd955be5c3e01e198de20d0782404e7f4eba64a2d29f71bc",
     "third_party/opengnm-psbc/src/compiler/nir/nir_intrinsics.h": "2d6f9e713863ce5a5fa51d4ed684896ce62fd347f289ae7a6c458d4b0069c8ee",
@@ -309,7 +309,7 @@
   }
 }
 PS5_GLSL_RECEIPT_END */
-#define PS5_GLSL_RECEIPT_SHA256 "b07b3e71b4a846cc6eea1bc7c30c8239e933940b09421ffdb1a10879a36bb3d3"
+#define PS5_GLSL_RECEIPT_SHA256 "a9973f0fc9cf5ce2cb6446776d29b6d879397ad7c4b4e14720c14e642bdda1ab"
 #define PS5_GLSL_DEFAULT_BYTES 16u
 #define PS5_GLSL_ADDEND_OFFSET 0u
 _Static_assert(PS5_GLSL_DEFAULT_BYTES > 0 && PS5_GLSL_DEFAULT_BYTES <= 64 &&
